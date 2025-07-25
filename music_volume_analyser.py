b09e8ef288b309550ee7c9027a95f219ccd8bec6
Anat Wax <anatwax@gmail.com>
2025-07-26 00:46:53 +0300

    Refactor file handling and output organization
    - Added `mp3_list_to_analyze()` to centralize MP3 file filtering and improve efficiency
    - Added `perform_stft_analysis()` to centralize logic and eliminate code duplication
    - Added folder existence and file presence checks in `analysed_normalized_rms_dict()`
    - Added a `.gitignore` file to the project
    
    Signed-off-by: Anat Wax <anatwax@gmail.com>

--- music_volume_analyser.py
+++ music_volume_analyser.py
@@ -6,33 +6,43 @@ import numpy as np
 import librosa.display
 import matplotlib.pyplot as plt
 
-
-folder_path = r'H:\Documents\Raven\raven sounds'
+folder_path = r'H:\samuel\Raven\raven sounds'
 sound_rms_dict = {}
 audio_name_list = []
 sample_rate_list = []
 mark_open = {}
 
+def perform_stft_analysis(audio_path):
+    y, sr = librosa.load(audio_path, sr=None)
+    S, _ = librosa.magphase(librosa.stft(y))
+    return y, sr, S
+
+def mp3_list_to_analyze():
+    return [audio_file for audio_file in os.listdir(folder_path)
+             if os.path.isfile(os.path.join(folder_path, audio_file)) and audio_file.lower().endswith(".mp3")]
+
 
 def analysed_normalized_rms_dict(return_numpy_array=False):
+    if not os.path.isdir(folder_path):
+        raise FileNotFoundError(f"Folder not found in path: {folder_path}")
+    mp3_files = mp3_list_to_analyze()
+    if not mp3_files:
+        raise FileNotFoundError(f"No '.mp3' files found in specified path: {folder_path}")
     audio_data_dict = {}
     numpy_normalized_rms_dict = {}
-    for audio_file in os.listdir(folder_path):
-        if os.path.isfile(os.path.join(folder_path, audio_file)) and \
-                audio_file.lower().endswith(".mp3"):
-            audio_path = os.path.join(folder_path, audio_file)
-            y, sr = librosa.load(audio_path, sr=None)
-            S, phase = librosa.magphase(librosa.stft(y))
-            # Plot the normalized RMS values:
-            rms = librosa.feature.rms(S=S, hop_length=32)
-            # Apply min-max normalization:
-            normalized_rms = (rms - rms.min()) / (rms.max() - rms.min())
-            numpy_normalized_rms_dict[audio_file] = normalized_rms
-            audio_name_list.append(audio_file)
-            sample_rate_list.append(sr)
-            # Convert the numpy nd array into a list:
-            normalized_rms_list = normalized_rms[0].tolist()
-            sound_rms_dict[audio_path.split("\\")[-1]] = normalized_rms_list
+    for audio_file in mp3_files:
+        audio_path = os.path.join(folder_path, audio_file)
+        _, sr, S = perform_stft_analysis(audio_path)
+        # Plot the normalized RMS values:
+        rms = librosa.feature.rms(S=S, hop_length=32)
+        # Apply min-max normalization:
+        normalized_rms = (rms - rms.min()) / (rms.max() - rms.min())
+        numpy_normalized_rms_dict[audio_file] = normalized_rms
+        audio_name_list.append(audio_file)
+        sample_rate_list.append(sr)
+        # Convert the numpy nd array into a list:
+        normalized_rms_list = normalized_rms[0].tolist()
+        sound_rms_dict[audio_path.split("\\")[-1]] = normalized_rms_list
     if return_numpy_array:
         return numpy_normalized_rms_dict
     for audio_name_index, track_name in enumerate(audio_name_list):
@@ -147,48 +157,45 @@ def generate_graphs(generate_rms=True, generate_power=False, generate_volume=Fal
         print("Didn't generate any graphs, no parameters were selected")
         return
     rms_dict = analysed_normalized_rms_dict(return_numpy_array=True)
-    for folder_path_index, audio_file in enumerate(os.listdir(folder_path)):
-        if os.path.isfile(os.path.join(folder_path, audio_file)) and \
-                audio_file.lower().endswith(".mp3"):
-            audio_path = os.path.join(folder_path, audio_file)
-            fig, ax = plt.subplots(nrows=num_arguments_passed, sharex=True)
-            rms = rms_dict[audio_file]
-            graph_index = 0
-            times = librosa.times_like(rms)
-            enumerate_of_rms_values = np.arange(rms.shape[1])
-            if num_arguments_passed > 1:
-                if generate_rms:
-                    ax[graph_index].semilogy(times, rms[0], label='RMS Energy')
-                    ax[graph_index].semilogy(enumerate_of_rms_values, rms[0], label='RMS Energy')
-                    ax[graph_index].set_xticks(enumerate_of_rms_values[::10])
-                    ax[graph_index].set_xticklabels(enumerate_of_rms_values[::10])
-                    graph_index += 1
-                if generate_power:
-                    y, sr = librosa.load(audio_path, sr=None)
-                    S, phase = librosa.magphase(librosa.stft(y))
-                    librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax[graph_index])
-                    ax[graph_index].set(title='log Power spectrogram')
-                    librosa.magphase(librosa.stft(y, window=np.ones, center=False))[0]
-                    graph_index += 1
-                if generate_volume:
-                    volume = librosa.amplitude_to_db(rms, ref=np.max)
-                    ax[graph_index].plot(times, volume[0], label='Volume')
-                    ax[graph_index].set_xlim([times.min(), times.max()])
-                    ax[graph_index].set_ylabel('Volume')
-                    ax[graph_index].legend()
-            else:
-                if not generate_rms:
-                    print("Can only generate a single graph with RMS values, and this is not what was requested")
-                    return
-                ax.semilogy(enumerate_of_rms_values, rms[0], label='RMS Energy')
-                ax.set_xticks(enumerate_of_rms_values[::10])
-                ax.set_xticklabels(enumerate_of_rms_values[::10])
-            plt.savefig(audio_path.split("\\")[-1].replace("mp3", "png"))
-            # plt.figure(figsize=(12, 4), num=audio_file)
-            # librosa.display.waveshow(y, sr=sr)
-            plt.title(audio_file)
+    for folder_path_index, audio_file in enumerate(mp3_list_to_analyze()):
+        audio_path = os.path.join(folder_path, audio_file)
+        fig, ax = plt.subplots(nrows=num_arguments_passed, sharex=True)
+        rms = rms_dict[audio_file]
+        graph_index = 0
+        times = librosa.times_like(rms)
+        enumerate_of_rms_values = np.arange(rms.shape[1])
+        if num_arguments_passed > 1:
+            if generate_rms:
+                ax[graph_index].semilogy(times, rms[0], label='RMS Energy')
+                ax[graph_index].semilogy(enumerate_of_rms_values, rms[0], label='RMS Energy')
+                ax[graph_index].set_xticks(enumerate_of_rms_values[::10])
+                ax[graph_index].set_xticklabels(enumerate_of_rms_values[::10])
+                graph_index += 1
+            if generate_power:
+                y, _, S = perform_stft_analysis(audio_path)
+                librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax[graph_index])
+                ax[graph_index].set(title='log Power spectrogram')
+                librosa.magphase(librosa.stft(y, window=np.ones, center=False))[0]
+                graph_index += 1
+            if generate_volume:
+                volume = librosa.amplitude_to_db(rms, ref=np.max)
+                ax[graph_index].plot(times, volume[0], label='Volume')
+                ax[graph_index].set_xlim([times.min(), times.max()])
+                ax[graph_index].set_ylabel('Volume')
+                ax[graph_index].legend()
+        else:
+            if not generate_rms:
+                print("Can only generate a single graph with RMS values, and this is not what was requested")
+                return
+            ax.semilogy(enumerate_of_rms_values, rms[0], label='RMS Energy')
+            ax.set_xticks(enumerate_of_rms_values[::10])
+            ax.set_xticklabels(enumerate_of_rms_values[::10])
+        plt.savefig(audio_path.split("\\")[-1].replace("mp3", "png"))
+        # plt.figure(figsize=(12, 4), num=audio_file)
+        # librosa.display.waveshow(y, sr=sr)
+        plt.title(audio_file)
 
-            plt.show()
+        plt.show()
 
 
 def main():
